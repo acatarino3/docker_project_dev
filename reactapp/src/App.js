562d02a14f21e64da0759460a7c3ba2c49cb37dd
acatarino3 <acatarino3@gatech.edu>
2024-04-02 00:04:53 -0400

    Added reactapp and docker config to repo

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+  }, [])
+
+  return (
+    <div >
+    </div>
+  );
+}
+
+export default App;
